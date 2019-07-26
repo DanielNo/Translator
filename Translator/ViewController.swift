c7d0b2eb17e954a8e1e79452a80f6a72c6b97ea3
Daniel No <dno@webmd.net>
2019-07-26 15:40:28 -0400

    init

---
+++ Translator/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Translator
+//
+//  Created by No, Daniel on 7/26/19.
+//  Copyright © 2019 No, Daniel. All rights reserved.
+//
+
+import UIKit
+import Rswift
+
+class ViewController: UIViewController {
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bigBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    func testFunction(){
+        let image = R.image.walgreens()
+        
+    }
+
+
+}
+
